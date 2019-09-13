4d15a01a839d62039df30781cc9ca0d5d3c5d8d2
Maxwell Da Silva <dayvson@gmail.com>
2019-09-13 00:38:51 -0400

    adding motionDevice test

---
+++ motionDevice/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+let value = 0;
+
+function draw() {
+  fill(value);
+  rect(25, 25, 50, 50);
+}
+function deviceMoved() {
+  value = value + 5;
+  if (value > 255) {
+    value = 0;
+  }
+}
